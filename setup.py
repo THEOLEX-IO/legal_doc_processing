c3719792384a9f6284c048774a479d944e3842c0
Jawad <jawad.alaoui@gmail.com>
2021-03-19 22:57:25 +0100

    init

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from document_processing.version import Version
+
+
+setuptools.setup(name='theolex-document-processing',
+                 version=Version('1.0.0').number,
+                 description='Theolex document processing',
+                 long_description=open('README.md').read().strip(),
+                 author='Jawad Alaoui',
+                 author_email='jawad@theolex.io',
+                 url='http://theolex-document-processing',
+                 py_modules=['document_processing'],
+                 install_requires=[],
+                 license='MIT License',
+                 zip_safe=False,
+                 keywords='theolex document processing package',
+                 classifiers=['Packages', 'NLP'])

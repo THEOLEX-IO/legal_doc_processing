b8f5af88540daced0c0537b357214ec1be1d6aa7
khaledha <khaledhadjali1@gmail.com>
2021-05-06 13:08:08 +0100

    fix

--- create_package.sh
+++ create_package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pip install check-wheel-contents
+pip install wheel-inspect
 
 echo "----------- create package using wheel ------------"  
 python setup.py bdist_wheel
@@ -8,6 +9,9 @@ echo "------------ package has been created -----------"
 
 echo '-------- check package healthy ------------'
 check-wheel-contents dist/theolex_document_processing*
+
+echo "json file presentation for the library"
+wheel2json dist/theolex_document_processing*
 
 echo "--------- try pip install wheel file -----------"
 pip  install dist/theolex_document_processing*

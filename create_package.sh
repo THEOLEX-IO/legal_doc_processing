3cfa5094e72cae054a0f35d39bd03a8e6922091d
khaledha <khaledhadjali1@gmail.com>
2021-05-06 13:02:43 +0100

    create wheel package

---
+++ create_package.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "----------- create package using wheel ------------"  
+python setup.py bdist_wheel
+echo "------------ package has been created -----------"
+
+echo '-------- check package healthy ------------'
+check-wheel-contents dist/theolex_document_processing*
+
+echo "--------- try pip install wheel file -----------"
+pip  install dist/theolex_document_processing*

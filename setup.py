b26c646ffbd84c4578c625e652a36f5aab008df1
khaledha <khaledhadjali1@gmail.com>
2021-05-07 22:52:25 +0100

    change package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from legal_doc_processing.version import Version
 
 
-setuptools.setup(name='theolex-document-processing',
+setuptools.setup(name='legal_doc_processing',
                  version=Version('1.0.0').number,
                  description='Theolex document processing',
                  long_description=open('README.md').read().strip(),

532c8baa67c4d049cd0ff1b60fb090c1837af09b
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-05-17 17:11:55 +0200

    add funct_test on dataset files

---
+++ tests/funct/test_dataset.py
@@ -0,0 +1,37 @@
+import pytest
+import os
+import json
+import pandas as pd
+
+
+def test_find_features_data(root="./tests/dataset/features"):
+    """just test to open the files in feature folder"""
+
+    for text in os.listdir(root):
+
+        with open(root + "/" + text, "r") as f:
+            txt = f.read()
+
+
+def test_make_features_dataframe(root="./tests/dataset/features"):
+    """using various texts to make a dataframe """
+
+    data_list = []
+    for text in os.listdir(root):
+
+        with open(root + "/" + text, "r") as f:
+            data_list.append({"file_key": text, "text": f.read()})
+
+        df = pd.DataFrame(data_list)
+
+    return df
+
+
+def test_find_features_labels(root="./tests/dataset/labels"):
+    """just test to open the files in feature folder"""
+
+    for text in os.listdir(root):
+
+        with open(root + "/" + text, "r") as f:
+            txt = f.read()
+            js = json.loads(txt)

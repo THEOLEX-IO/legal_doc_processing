37dd99f4cd5895f6154e61c23ded02db3724d18c
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-05-17 11:37:06 +0200

    even more pythonic code

--- tests/tests_helper.py
+++ tests/tests_helper.py
@@ -4,7 +4,4 @@ import tests.helpers
 def list_has(value, lst):
     """search a value in a list, return a boolean True if found else False """
 
-    for val in lst:
-        if val == value:
-            return True
-    return False
+    return bool([1 for i in lst if i == value])

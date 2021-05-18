be0c6b824069cfb5f9eb6059b29ed92104cc36ca
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-05-18 22:46:05 +0200

    import legal doc in legal_doc_processing

--- legal_doc_processing/__init__.py
+++ legal_doc_processing/__init__.py
@@ -1,1 +1,4 @@
 """This module implements NLP methods to handle legal documentation. From text cleaning to information extraction, it centralizes main functionalities that simplify the work for text mining for this category of documents"""
+
+
+from legal_doc_processing.legal_doc import LegalDoc

6269d22bf6fe0550eb7a98efa00737e21dbf297a
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-05-18 20:52:16 +0200

    replace roots by labels_root and features_root

--- tests/funct/test_dataset.py
+++ tests/funct/test_dataset.py
@@ -3,21 +3,26 @@ import json
 
 import pytest
 
-from tests.funct.utils import make_features_dataframe, make_labels_dataframe
+from tests.funct.utils import (
+    make_features_dataframe,
+    make_labels_dataframe,
+    features_root,
+    labels_root,
+)
 
 
 class TestFeatures:
     """Test class for features"""
 
-    def test_find_features_data(self, root="./tests/dataset/features"):
+    def test_find_features_data(self, root=features_root):
         """just test to open the files in feature folder"""
 
         for text in os.listdir(root):
 
-            with open(root + "/" + text, "r") as f:
+            with open(root + text, "r") as f:
                 txt = f.read()
 
-    def test_make_features_dataframe(self, root="./tests/dataset/features"):
+    def test_make_features_dataframe(self, root=features_root):
         """using various texts to make a dataframe """
 
         make_features_dataframe(root)
@@ -26,7 +31,7 @@ class TestFeatures:
 class Testlabels:
     """Test class for flabels"""
 
-    def test_find_labels_data(self, root="./tests/dataset/labels"):
+    def test_find_labels_data(self, root=labels_root):
         """just test to open the files in feature folder"""
 
         for text in os.listdir(root):
@@ -35,7 +40,7 @@ class Testlabels:
                 txt = f.read()
                 js = json.loads(txt)
 
-    def test_make_labels_dataframe(self, root="./tests/dataset/labels"):
+    def test_make_labels_dataframe(self, root=labels_root):
         """using various texts to make a dataframe """
 
         make_labels_dataframe(root)

89ffa83b3c9271586e2b4edecaa63351c1a50764
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-05-19 21:27:33 +0200

    pipeline and first page

--- legal_doc_processing/information_extraction.py
+++ legal_doc_processing/information_extraction.py
@@ -3,6 +3,8 @@ import re
 import spacy
 from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
 
+from legal_doc_processing.utils import clean_doc
+
 nlp = spacy.load("en_core_web_sm")
 
 
@@ -13,15 +15,15 @@ nlp = pipeline(
 )
 
 
-def get_case(text):
-    for line in text:
+def get_case(first_page):
+    for line in first_page:
         if len(line) < 40:
             p = re.compile("NO[\.:]\s*.+")
             result = p.search(line.upper())
             if result:
                 return result.group(0).strip()
 
-    for line in text:
+    for line in first_page:
         if len(line) < 40:
             p = re.compile("\d*-?CV-\d+.*")
             result = p.search(line.upper())

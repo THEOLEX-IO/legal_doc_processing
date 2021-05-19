fcf9504e958b25219933d4bfc500ff06fd99a0a0
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-05-19 23:58:53 +0200

    add a dummy but working get_case

--- legal_doc_processing/information_extraction.py
+++ legal_doc_processing/information_extraction.py
@@ -8,7 +8,17 @@ from legal_doc_processing.utils import clean_doc
 # nlp = spacy.load("en_core_web_sm")
 
 
-def get_case(first_page):
+def alex_get_case(article_text: str) -> str:
+    """extract the case from a text; return str """
+
+    txt = article_text[:50]
+    if "Case " in txt:
+        return txt.split(" ")[1]
+
+    return "-- NONE --"
+
+
+def jawad_get_case(first_page):
     for line in first_page:
         if len(line) < 40:
             p = re.compile("NO[\.:]\s*.+")
@@ -32,7 +42,7 @@ def get_defendant(formatted_article_text):
         tokenizer="distilbert-base-cased",
     )
 
-    # first_page = [text for text in clean_text[0] if len(text) > 5]
+    # first_page = [text for text in formatted_article_text[0] if len(text) > 5]
 
     pred = nlp(question="Who is the defendant?", context=formatted_article_text, topk=2)
     return pred[0]["answer"]

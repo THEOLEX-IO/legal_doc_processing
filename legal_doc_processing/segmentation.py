6120a7e1ced934fe830a00a13dbd8114bc09d97a
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-05-20 12:30:22 +0200

    add insert funct in segementation

--- legal_doc_processing/segmentation.py
+++ legal_doc_processing/segmentation.py
@@ -3,36 +3,39 @@ from legal_doc_processing import utils
 
 
 def clean_doc(file_text):
-    pages = []
+    """ """
 
+    pages = []
     for page in file_text.split("\x0c"):
-        # clean text
-        page_meta = [{'text': clean(para)}
-                     for para in page.split('\n')]
+
+        # clen text
+        page_meta = [
+            {"text": clean(para, lower=False, no_line_breaks=True).replace("_", "")}
+            for para in page.split("\n")
+        ]
+
         clean_page = []
         previous_line = {}
         text = ""
+
         # add meta data
         for line in page_meta:
-            line['is_section_num'] = utils.is_section_num(str(line['text']))
-            line['is_title'] = utils.is_title(str(line['text']))
-            line['ends_with_ponc'] = utils.ends_with_ponc(str(line['text']))
-            line['is_alpha'] = sum(c.isalpha() for c in str(line['text']))
-            line['start_with_upper'] = utils.start_with_upper(str(line['text']))
-
+            line["is_section_num"] = utils.is_section_num(line["text"])
+            line["is_title"] = utils.is_title(line["text"])
+            line["ends_with_ponc"] = utils.ends_with_ponc(line["text"])
+            line["is_alpha"] = sum(c.isalpha() for c in line["text"])
+            line["start_with_upper"] = utils.start_with_upper(line["text"])
             # not relevant line
-            if not line['is_alpha']:
+            if not line["is_alpha"]:
                 continue
             if not utils.same_sentence(previous_line, line):
-                # if text:
-                clean_page.append(text)
-                text = ""
+                if text:
+                    clean_page.append(text)
+                    text = ""
             previous_line = line
-            text = " ".join([text, str(line['text'])])
-        if clean_page:
-            pages.append(clean_page)
-    return pages
+            text = " ".join([text, line["text"]])
 
-    # generate documents with headers and paragraphs
-    def get_structured_document(file_text):
-        return file_text # [{"header": "INTRODUCTION", "content": "The Commodity Futures Trading Commission...","id":1},...]
+        if len(clean_page):
+            pages.append(clean_page)
+
+    return pages

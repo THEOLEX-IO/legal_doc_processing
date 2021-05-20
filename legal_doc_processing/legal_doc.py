a771f6a86ed1f0e2fd7f81b1eb2cd104cc9921d4
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-05-20 13:56:23 +0200

    del commented useless code

--- legal_doc_processing/legal_doc.py
+++ legal_doc_processing/legal_doc.py
@@ -1,7 +1,6 @@
 import os
 
 
-# from legal_doc_processing.utils import clean_spec_chars, clean_doc
 import legal_doc_processing.information_extraction as infext
 import legal_doc_processing.segmentation as seg
 
@@ -23,25 +22,16 @@ class LegalDoc:
         # raw text
         self.file_path = None
         self.raw_text = text
-        # self.article_text, self.formatted_article_text = clean_spec_chars(text)
 
+        # self.article_text, self.formatted_article_text = clean_spec_chars(text)
         # self.clean_text = None
 
         # features
         self.case = None
         self.defendant = None
 
-    # def clean(self) -> str:
-    #     """clean the text """
-
-    #     self.clean_text = clean_doc(self.raw_text)
-
-    #     return self.clean_text
-
     def predict_case(self) -> str:
         """predict case, update self.case attr and return the value"""
-
-        # self.case = infext.alex_get_case(self.article_text)
 
         clean_pages = seg.clean_doc(self.raw_text)
         self.case = infext.get_case(clean_pages[0])
@@ -51,11 +41,7 @@ class LegalDoc:
     def predict_defendant(self) -> str:
         """predict defendant, update self.defendant attr and return the value"""
 
-        # self.defendant = infext.get_defendant(self.formatted_article_text)
-
         self.defendant = "--NONE--"
-
-        # self.defendant = infext.get_defendant(self.formatted_article_text)
 
         return self.defendant
 

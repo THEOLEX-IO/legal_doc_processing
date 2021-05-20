0a743e578cc92d9fa292b54347bdfde3ac5aee8a
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-05-20 12:29:26 +0200

    comment everything not 100% usefull

--- legal_doc_processing/utils.py
+++ legal_doc_processing/utils.py
@@ -9,10 +9,10 @@ from cleantext import clean
 stopwords = nltk.corpus.stopwords.words("english")
 
 
-def hello():
-    """dummy  """
+# def hello():
+#     """dummy  """
 
-    return "world"
+#     return "world"
 
 
 def load_data(file_path: str) -> str:
@@ -24,34 +24,34 @@ def load_data(file_path: str) -> str:
     return txt
 
 
-def clean_spec_chars(text: str) -> tuple:
-    """first text cleaning based on regex, just keep text not spec chars
-    return tupple of text"""
+# def clean_spec_chars(text: str) -> tuple:
+#     """first text cleaning based on regex, just keep text not spec chars
+#     return tupple of text"""
 
-    # article text
-    article_text = re.sub(r"\[[0-9]*\]", " ", text)
-    article_text = re.sub(r"\s+", " ", article_text)
+#     # article text
+#     article_text = re.sub(r"\[[0-9]*\]", " ", text)
+#     article_text = re.sub(r"\s+", " ", article_text)
 
-    # formated text
-    formatted_article_text = re.sub("[^a-zA-Z]", " ", article_text)
-    formatted_article_text = re.sub(r"\s+", " ", formatted_article_text)
+#     # formated text
+#     formatted_article_text = re.sub("[^a-zA-Z]", " ", article_text)
+#     formatted_article_text = re.sub(r"\s+", " ", formatted_article_text)
 
-    return article_text, formatted_article_text
+#     return article_text, formatted_article_text
 
 
-def handle_encoding(text: str) -> str:
-    """handle encoding problems and force ascii conversion ; return clean text """
+# def handle_encoding(text: str) -> str:
+#     """handle encoding problems and force ascii conversion ; return clean text """
 
-    # encoding the text to ASCII format
-    text_encode = text.encode(encoding="ascii", errors="ignore")
+#     # encoding the text to ASCII format
+#     text_encode = text.encode(encoding="ascii", errors="ignore")
 
-    # decoding the text
-    text_decode = text_encode.decode()
+#     # decoding the text
+#     text_decode = text_encode.decode()
 
-    # cleaning the text to remove extra whitespace
-    clean_text = " ".join([word for word in text_decode.split()])
+#     # cleaning the text to remove extra whitespace
+#     clean_text = " ".join([word for word in text_decode.split()])
 
-    return clean_text
+#     return clean_text
 
 
 def is_section_num(text: str) -> bool:
@@ -107,75 +107,43 @@ def same_sentence(sent1, sent2):
     return True
 
 
-def clean_doc(file_text):
-    pages = []
-    for page in file_text.split("\x0c"):
-        # clen text
-        page_meta = [
-            {"text": clean(para, lower=False, no_line_breaks=True).replace("_", "")}
-            for para in page.split("\n")
-        ]
-        clean_page = []
-        previous_line = {}
-        text = ""
-        # add meta data
-        for line in page_meta:
-            line["is_section_num"] = is_section_num(line["text"])
-            line["is_title"] = is_title(line["text"])
-            line["ends_with_ponc"] = ends_with_ponc(line["text"])
-            line["is_alpha"] = sum(c.isalpha() for c in line["text"])
-            line["start_with_upper"] = start_with_upper(line["text"])
-            # not relevant line
-            if not line["is_alpha"]:
-                continue
-            if not same_sentence(previous_line, line):
-                if text:
-                    clean_page.append(text)
-                    text = ""
-            previous_line = line
-            text = " ".join([text, line["text"]])
-        if len(clean_page):
-            pages.append(clean_page)
-    return pages
+# def get_structured_document(file):
+
+#     text_structured = []
+#     file_cleaned = clean_doc(file)
+#     i = 0
+#     for pages in file_cleaned:
+#         text_ = {}
+#         text_["content"] = pages
+#         text_["id"] = i
+#         if is_title(pages[0]):
+#             text_["header"] = pages[0]
+
+#         text_structured.append(text_)
+#         i += 1
+
+#     return text_structured
 
 
-def get_structured_document(file):
-
-    text_structured = []
-    file_cleaned = clean_doc(file)
-    i = 0
-    for pages in file_cleaned:
-        text_ = {}
-        text_["content"] = pages
-        text_["id"] = i
-        if is_title(pages[0]):
-            text_["header"] = pages[0]
-
-        text_structured.append(text_)
-        i += 1
-
-    return text_structured
+# def word_frequency(text):
+#     word_frequencies = {}
+#     for word in nltk.word_tokenize(text):
+#         if word not in stopwords:
+#             if word not in word_frequencies.keys():
+#                 word_frequencies[word] = 1
+#             else:
+#                 word_frequencies[word] += 1
+#     return word_frequencies
 
 
-def word_frequency(text):
-    word_frequencies = {}
-    for word in nltk.word_tokenize(text):
-        if word not in stopwords:
-            if word not in word_frequencies.keys():
-                word_frequencies[word] = 1
-            else:
-                word_frequencies[word] += 1
-    return word_frequencies
-
-
-def sentence_score(sentence_list, word_frequencies):
-    sentence_scores = {}
-    for sent in sentence_list:
-        for word in nltk.word_tokenize(sent.lower()):
-            if word in word_frequencies.keys():
-                if len(sent.split(" ")) < 30:
-                    if sent not in sentence_scores.keys():
-                        sentence_scores[sent] = word_frequencies[word]
-                    else:
-                        sentence_scores[sent] += word_frequencies[word]
-    return sentence_scores
+# def sentence_score(sentence_list, word_frequencies):
+#     sentence_scores = {}
+#     for sent in sentence_list:
+#         for word in nltk.word_tokenize(sent.lower()):
+#             if word in word_frequencies.keys():
+#                 if len(sent.split(" ")) < 30:
+#                     if sent not in sentence_scores.keys():
+#                         sentence_scores[sent] = word_frequencies[word]
+#                     else:
+#                         sentence_scores[sent] += word_frequencies[word]
+#     return sentence_scores

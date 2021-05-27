ab7001818389e1713619ea30ea8751bb69d0b7c7
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-05-27 09:44:16 +0200

    read a corpus of folder, not one file

--- notebooks/get_case.py
+++ notebooks/get_case.py
@@ -25,7 +25,7 @@ from notebooks.paths import *
 
 
 # read file
-raw_text = load_data(one_file_path)
+raw_text = load_data(one_file_order)
 raw_text[:300]
 
 
@@ -44,23 +44,20 @@ case
 
 
 # file_path
-folder = "tests/dataset/features"
-_files_path = os.getcwd() + "/" + folder
-files_path_list = [_files_path + "/" + filename for filename in os.listdir(_files_path)]
-files_path_list
-
+file_path_list = x_data_files(20, "order")
 
 # read file
-raw_text_list = [load_data(file_path) for file_path in files_path_list]
+raw_text_list = [load_data(file_path) for file_path in file_path_list]
 raw_text_list[0]
-
 
 # clean and first
 clean_pages_list = [clean_doc(raw_text) for raw_text in raw_text_list]
 first_page_list = [pages[0] for pages in clean_pages_list]
 first_page_list[0]
 
-
 # pred case
 case_list = [get_case(fp) for fp in first_page_list]
 case_list
+
+
+list(zip(file_path_list, case_list))

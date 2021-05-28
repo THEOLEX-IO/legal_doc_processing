022cc1b00ff9a823c14738ab29b66a7e662540ee
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-05-28 23:31:25 +0200

    test commit

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 import setuptools
 from legal_doc_processing.version import Version
 
-REQUIRED = [
-    'pandas','nltk','transformers', 'spacy'
-]
+REQUIRED = ["pandas", "nltk", "transformers", "spacy"]
 
-setuptools.setup(name='legal_doc_processing',
-                 version=Version('1.0.0').number,
-                 description='Theolex document processing',
-                 long_description=open('README.md').read().strip(),
-                 long_description_content_type='text/x-rst',
-                 author='Jawad Alaoui',
-                 author_email='jawad@theolex.io',
-                 url='http://theolex-document-processing',
-                 packages=setuptools.find_packages(),
-                 include_package_data=True,
-                 install_requires=REQUIRED,
-                 license='MIT License',
-                 zip_safe=False,
-                 keywords='theolex document processing package',
-                #  classifiers=['Packages', 'NLP']
-                )
+setuptools.setup(
+    name="legal_doc_processing",
+    version=Version("0.0.0").number,
+    description="Theolex document processing",
+    long_description=open("README.md").read().strip(),
+    long_description_content_type="text/x-rst",
+    author="Jawad Alaoui",
+    author_email="jawad@theolex.io",
+    url="http://theolex-document-processing",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=REQUIRED,
+    license="MIT License",
+    zip_safe=False,
+    keywords="theolex document processing package",
+    #  classifiers=['Packages', 'NLP']
+)

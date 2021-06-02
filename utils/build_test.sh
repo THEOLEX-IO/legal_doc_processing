cbc7e9ae97c2a235ba0e08270e326ac532052438
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-06-02 14:01:17 +0200

    simplify dockers

---
+++ utils/build_test.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+docker build --no-cache -f ./utils/Dockerfile -t asyoez/legal_doc_processing_repo:latest .
+
+docker run -ti --entrypoint pytest asyoez/legal_doc_processing_repo:latest
--- utils/build_test_push.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-docker build --no-cache -f ./utils/Dockerfile.test -t asyoez/legal_doc_processing_repo:latest
-docker run -ti docker push asyoez/legal_doc_processing_repo:latest
-docker push asyoez/legal_doc_processing_repo:latest
-
-

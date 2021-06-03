b23d8f37161cf65afdf4da9f149a80910a709a8c
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-06-03 23:51:01 +0200

    fix

--- utils/init.sh
+++ utils/init.sh
@@ -3,11 +3,7 @@
 
 
 # dowload collections
-<<<<<<< HEAD
-python -c "import nltk; import spacy; nltk.download('stopwords'); nltk.download('popular'); nlp = spacy.load('en_core_web_sm')"
-=======
 python -c "import nltk; import spacy; nltk.download('stopwords'); nltk.download('popular'); "
->>>>>>> release/v0.1.6.1
 
 # bootsrap all
 python -c "from legal_doc_processing import LegalDoc; LegalDoc('hello world')"

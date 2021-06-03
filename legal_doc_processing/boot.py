1b0ffb7600fc555548beeb30ad77f8bf5ab0c018
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-06-03 23:42:40 +0200

    ok for 1.0.1

---
+++ legal_doc_processing/boot.py
@@ -0,0 +1,5 @@
+def boot():
+
+    from legal_doc_processing.legal_doc import LegalDoc
+
+    hello = LegalDoc("Hello World")
--- utils/pypi.sh
+++ utils/pypi.sh
@@ -12,8 +12,14 @@ twine upload -u $TWINE_USER -p $TWINE_PASSWORD dist/*
 
 
 
-# docker run python:3.9-buster
+# instructions
+# docker run -ti python:3.9-buster /bin/bash 
+# pip install ipython legal-doc-processing==0.1.6.4
 
+# ipython 
+# from legal_doc_processing import boot
+# boot()
 
+# TESTPypu
 # twine upload --repository testpypi dist/*
 # python3 -m pip install --index-url https://test.pypi.org/simple/ your-package

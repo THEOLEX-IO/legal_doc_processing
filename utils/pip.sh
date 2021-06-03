734689e742b6e7015bfc7d519842174314b323f9
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-06-03 11:37:45 +0200

    pip

--- utils/pip.sh
+++ utils/pip.sh
@@ -2,5 +2,7 @@
 
 docker build --no-cache -f utils/Dockerfile.pip -t python3-pip:latest .
 
+docker run -ti python3-pip:latest ipython
+#  "from legal_doc_processing import LegalDoc; LegalDoc('hello world')"
 
 

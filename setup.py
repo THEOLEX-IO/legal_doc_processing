144ddd9308f126ec802142f33a56e5dd181c17fd
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-06-03 13:19:30 +0200

    version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ REQUIRED = [
 
 setuptools.setup(
     name="legal_doc_processing",
-    version=Version("v0.1.4").number,
+    version=Version("v0.1.6").number,
     description="Theolex document processing",
     long_description=open("README.md").read().strip(),
     long_description_content_type="text/x-rst",

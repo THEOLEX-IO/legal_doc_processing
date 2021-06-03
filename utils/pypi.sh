0cc967917e7f86e9fb069de163773205434211bd
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-06-03 22:20:54 +0200

    working

--- utils/pypi.sh
+++ utils/pypi.sh
@@ -11,4 +11,9 @@ python setup.py sdist
 twine upload -u $TWINE_USER -p $TWINE_PASSWORD dist/*
 
 
+
 # docker run python:3.9-buster
+
+
+# twine upload --repository testpypi dist/*
+# python3 -m pip install --index-url https://test.pypi.org/simple/ your-package

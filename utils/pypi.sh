cc839f3c8ba6d80e919ac2cdbfbbe8144eadb927
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-06-07 11:26:13 +0200

    fix

--- utils/pypi.sh
+++ utils/pypi.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # clean dist
-sudo rm -f dist/*
-sudo rm -rf dist/*
+
+sudo rm -rf dist
 
 # make dist
 python setup.py sdist
@@ -10,15 +10,14 @@ python setup.py sdist
 # push
 twine upload -u $TWINE_USER -p $TWINE_PASSWORD dist/*
 
-
-
 # instructions
 # docker run -ti python:3.9-buster /bin/bash 
 # pip install ipython legal-doc-processing==1.1.4
 
 # ipython 
-# from legal_doc_processing import boot
-# boot()
+# from legal_doc_processing import boot ; boot()
+
+
 
 # TESTPypu
 # twine upload -u $TWINE_USER -p $TWINE_PASSWORD --repository testpypi dist/*

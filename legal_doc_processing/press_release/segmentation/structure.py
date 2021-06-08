d33f66673ad652c9e64fd3a442fadf4ca14fc976
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-06-08 23:09:28 +0200

    add shall not pass

--- legal_doc_processing/press_release/segmentation/structure.py
+++ legal_doc_processing/press_release/segmentation/structure.py
@@ -5,6 +5,40 @@ from pprint import pformat, pprint
 sep = "---------------------"
 
 
+def _shall_not_pass(structured_text):
+
+    error = 0
+    # len id
+    if (len(structured_text["id"]) > 30) or ("--ERROR-- " in structured_text["id"]):
+        structured_text["id"] = "--ERROR-- " + structured_text["id"]
+        error += 1
+
+    # len date
+    if (len(structured_text["date"]) > 30) or ("--ERROR-- " in structured_text["date"]):
+        structured_text["date"] = "--ERROR-- " + structured_text["date"]
+        error += 1
+
+    #  Washington in h1 or len h1
+    if (
+        ("Washington, D" in structured_text["h1"][:19])
+        or (len(structured_text["h1"]) > 1000)
+        or ("--ERROR-- " in structured_text["h1"])
+    ):
+        structured_text["h1"] = "--ERROR-- " + structured_text["h1"]
+        error += 1
+
+    # Washington in article
+    if ("Washington, D" not in structured_text["article"][:19]) or (
+        "--ERROR-- " in structured_text["article"]
+    ):
+        structured_text["article"] = "--ERROR-- " + structured_text["article"]
+        error += 1
+
+    structured_text["error"] = error
+
+    return structured_text
+
+
 def _double_break_as_para(txt: str, sep: str = "---------------------") -> str:
     """add a --------- instdead of double break """
 
@@ -103,26 +137,34 @@ def structure_press_release(
         lines = [_squeeze_fake_break(txt) for txt in lines]
 
     if force_dict:
-        lines = _build_press_release(lines)
+        dd = _build_press_release(lines)
 
-    return lines
+    # errors
+    dd = _shall_not_pass(dd)
+
+    return dd
 
 
 if __name__ == "__main__":
 
     # import
-    import pandas as pd
-    from legal_doc_processing.press_release.utils import *
+    from legal_doc_processing.utils import get_pipeline
+    from legal_doc_processing.press_release.utils import press_release_X_y
+    from legal_doc_processing.press_release.segmentation.structure import (
+        structure_press_release,
+    )
+
+    # pipe
+    nlpipe = get_pipeline()
 
     # structured_press_release_list
-    press_txt_list = load_press_release_text_list()
-    structured_press_release_list = [structure_press_release(i) for i in press_txt_list]
+    df = press_release_X_y(features="defendant")
+    df["structured_txt"] = [structure_press_release(i) for i in df.txt.values]
 
-    # 1st
-    structured_press_release = structured_press_release_list[0]
+    one = df["structured_txt"].iloc[0]
+    one_list = [(k, str(v)[:100] + "...") for k, v in one.items()]
+    one_str = "\n".join([f"{i.rjust(10)}\t:\t{j}" for i, j in one_list])
+    print(one_str)
 
-    # others
-    df = pd.DataFrame(structured_press_release_list)
-    _df = df.copy()
-    for k in _df.columns:
-        _df[k] = df[k].apply(lambda i: str(i)[:12] + "...")
+    # df["id"] = df.structured_txt.apply(lambda i: i.get("id")[:100])
+    # df["date"] = df.structured_txt.apply(lambda i: i.get("date")[:100])

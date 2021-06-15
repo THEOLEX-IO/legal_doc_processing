a5d3d7499fc7575458f3e2ac1bde15fb7394d9d2
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-06-15 11:27:16 +0200

    wrtie cost

--- legal_doc_processing/press_release/information_extraction/cost.py
+++ legal_doc_processing/press_release/information_extraction/cost.py
@@ -1,9 +1,38 @@
+import os
+import copy
+
 import pandas as pd
+
 
 from legal_doc_processing.utils import (
     _if_not_pipe,
+    _if_not_spacy,
+    get_spacy,
     _ask,
+    get_label_,
 )
+
+
+def _get_entities_money(struct_doc: dict, n_paragraphs: int = 2, nlpspa=None) -> list:
+    """get entities MONEY from h1 and sub_article """
+
+    nlpspa = _if_not_spacy(nlpspa)
+
+    # sub article
+    sub_article = "\n".join(struct_doc["article"].split("\n")[:n_paragraphs])
+
+    # all pers all orgs from spacy entities
+    all_money = get_label_(struct_doc["h1"], "MONEY", nlpspa) + get_label_(
+        sub_article, "MONEY", nlpspa
+    )
+    all_money = list(set(all_money))
+
+    # clean
+    all_init_money = _cast_as_int(all_money)
+
+    all_init_money = list(set(all_init_money))
+
+    return all_init_money
 
 
 def _ask_all(txt, nlpipe) -> list:
@@ -31,28 +60,25 @@ def _ask_all(txt, nlpipe) -> list:
     # sort
     ans = sorted(ans, key=lambda i: i["score"], reverse=True)
 
-    # clean
-    ans = [i for i in ans if (i["answer"].lower() != "defendants")]
-
     return ans
 
 
-def _clean_ans(ans, threshold=0.5):
-    """ """
+# def _merge_ans(ans, threshold=0.5):
+#     """ """
 
-    # build dataframe
-    df = pd.DataFrame(ans)
-    df = df.loc[:, ["score", "answer"]]
+#     # build dataframe
+#     df = pd.DataFrame(ans)
+#     df = df.loc[:, ["score", "answer"]]
 
-    # group by ans and make cumutavie score of accuracy
-    ll = [
-        {"answer": k, "cum_score": v.score.sum()}
-        for k, v in df.groupby("answer")
-        if v.score.sum() > threshold
-    ]
-    ll = sorted(ll, key=lambda i: i["cum_score"], reverse=True)
+#     # group by ans and make cumutavie score of accuracy
+#     ll = [
+#         {"answer": k, "cum_score": v.score.sum()}
+#         for k, v in df.groupby("answer")
+#         if v.score.sum() > threshold
+#     ]
+#     ll = sorted(ll, key=lambda i: i["cum_score"], reverse=True)
 
-    return ll
+#     return ll
 
 
 def _cast_as_int(cleaned_ans):
@@ -89,6 +115,8 @@ def _cast_as_int(cleaned_ans):
     cleaned_ans_multi_2 = list()
     for numb, multi in cleaned_ans_multi:
         if not multi:
+            # dump centimies
+            numb = numb.split(".")[0]
             # easy, jsute keep the numbers
             numb = "".join([c for c in list(numb) if c.isnumeric()])
             numb = int(numb)
@@ -119,84 +147,85 @@ def _cast_as_int(cleaned_ans):
     return cleaned_ans_multi_2
 
 
-def predict_cost(structured_press_release: list, nlpipe=None):
+def predict_cost(struct_doc: list, nlpipe=None, nlpspa=None):
     """init a pipe if needed, then ask all questions and group all questions ans in a list sorted py accuracy """
 
-    # pipe
+    # pipe, spa
     nlpipe = _if_not_pipe(nlpipe)
+    nlpspa = _if_not_spacy(nlpspa)
 
-    # section cands
-    cands = [
-        structured_press_release["h1"].lower(),
-        structured_press_release["h2"].lower(),
-        # article = structured_press_release["article"].lower()
-    ]
+    # get_label_ h1
+    money_h1 = get_label_(struct_doc["h1"], "MONEY", nlpspa)
+    # print(f"money_h1 is {money_h1}")
+    money_h1_clean = list(set(_cast_as_int(money_h1)))
+    # print(f"money_h1_clean is {money_h1_clean}")
 
-    # section ok if "pay" or amend or $ or € or euro in the text
-    money = ["$", "€", "£", "euros", "dollar", "livre"]
-    is_good = (
-        lambda sec: ("amend" in sec)
-        or ("penalty" in sec)
-        or ("pay" in sec)
-        or sum([i for i in money if i.replace("s", "") in sec])
-    )
+    # cost in h1
+    if len(money_h1_clean) == 1:
+        return str(money_h1_clean[0])
 
-    # parse sections to know if ok
-    txt = ""
-    for section in cands:
-        txt = section if is_good(section) else ""
-        if txt:
-            break
+    if len(money_h1_clean) > 1:
+        return str(-2)
 
-    # if  nothing ok
-    if not txt:
-        return "--None--"
+    # get_label article
+    sub_article = "\n".join(struct_doc["article"].split("\n")[:2])
+    money_sub_article = get_label_(sub_article, "MONEY", nlpspa)
+    # print(f"money_sub_article is {money_sub_article}")
+    money_sub_article_clean = list(set(_cast_as_int(money_sub_article)))
+    # print(f"money_sub_article_clean is {money_sub_article_clean}")
 
-    # ask all and get all possible response
-    ans = _ask_all(txt, nlpipe)
+    # cost in article
+    if len(money_sub_article_clean) == 1:
+        return str(money_sub_article_clean[0])
+    elif len(money_sub_article_clean) > 1:
+        return str(max(money_sub_article_clean))
+    else:
+        return str(-1)
 
-    # group by ans, make cumulative sum of accuracy for eash ans and filter best ones
-    ll = _clean_ans(ans)
-
-    # keep ans
-    cleaned_ans = [i["answer"] for i in ll]
-
-    # cast as int
-    cleaned_ans = _cast_as_int(cleaned_ans)
-
-    # reponse
-    resp = ", ".join(cleaned_ans)
-
-    return resp
+    return str(-3)
 
 
+#     return resp
 if __name__ == "__main__":
 
     # import
-    from legal_doc_processing.utils import *
-    from legal_doc_processing.press_release.utils import load_press_release_text_list
+    from legal_doc_processing.utils import get_pipeline, get_spacy, get_label_
+    from legal_doc_processing.press_release.utils import press_release_X_y
     from legal_doc_processing.press_release.segmentation.structure import (
         structure_press_release,
     )
 
-    # pipe
+    # laod
     nlpipe = get_pipeline()
+    nlpspa = get_spacy()
 
-    # structured_press_release_list
-    press_txt_list = load_press_release_text_list()
-    structured_press_release_list = [structure_press_release(i) for i in press_txt_list]
+    # structured_press_release_r
+    df = press_release_X_y(features="cost")
+    df["structured_txt"] = [structure_press_release(i) for i in df.txt.values]
 
-    # test one
-    structured_press_release = structured_press_release_list[0]
+    # # one
+    # one = df.iloc[0, :]
+    # # one features
+    # cost = one.cost
+    # one_struct = struct_doc = one.structured_txt
+    # one_h1 = one_struct["h1"]
+    # one_article = one_struct["article"]
+    # sub_one_article = "\n".join(one_article.split("\n")[:2])
 
-    all_ans_h1 = _ask_all(structured_press_release["h1"], nlpipe)
-    all_ans_h2 = _ask_all(structured_press_release["h2"], nlpipe)
-    all_ans_article = _ask_all(structured_press_release["article"], nlpipe)
+    # # ents
+    # money_h1 = get_label_(one_h1, "MONEY", nlpspa)
+    # money_article = get_label_(sub_one_article, "MONEY", nlpspa)
 
-    ans = predict_cost(structured_press_release, nlpipe)
+    # 1 to len(df)
+    print(f" {'y'.rjust(30)} -->  {'pred'} \n")
+    print(160 * "-")
+    for i in range(0, len(df)):
+        cost = df.cost.iloc[i]
+        i_text = df.txt.iloc[i]
+        i_struct = df["structured_txt"].iloc[i]
 
-    # test others
-    ans_list = [predict_cost(p, nlpipe) for p in structured_press_release_list]
-
-    clean_ans_list = [[d["answer"] for d in ll] for ll in ans_list]
-    clean_ans_list = [", ".join(ll) for ll in clean_ans_list]
+        i_h1 = i_struct["h1"]
+        i_article = i_struct["article"]
+        sub_i_article = "\n".join(i_article.split("\n")[:2])
+        pred_ans = predict_cost(i_struct, nlpspa=nlpspa, nlpipe=nlpipe)
+        print(f" {str(cost).rjust(30)} --> pred : {pred_ans}")

636317b6095a79b08f0662359fb334d410258837
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-06-15 12:07:16 +0200

    fixes

--- legal_doc_processing/press_release/information_extraction/juridiction.py
+++ legal_doc_processing/press_release/information_extraction/juridiction.py
@@ -4,21 +4,24 @@ import pandas as pd
 
 from legal_doc_processing.utils import (
     _if_not_pipe,
-    _if_not_spacy, get_spacy,
+    _if_not_spacy,
+    get_spacy,
     _ask,
 )
 
-from legal_doc.predict_juridiction.utils import product_juridiction_pairs
+from legal_doc_processing.press_release.information_extraction.utils import (
+    product_juridiction_pairs,
+)
 
 
-def _filter_jur(txt, cands:list=None) : 
+def _filter_jur(txt, cands: list = None):
     """ """
 
-    if not cands : 
+    if not cands:
         cands = product_juridiction_pairs()
 
-    for k, v in cands.items() : 
-        if txt.lower().strip() == k.lower().strip() : 
+    for k, v in cands.items():
+        if txt.lower().strip() == k.lower().strip():
             return v.upper()
 
     return ""
@@ -36,16 +39,15 @@ def predict_juridiction(struct_doc: list, nlpipe=None, nlpspa=None):
     sub_article = "\n".join(struct_doc["article"].split("\n")[:2])
 
     # token filter h1
-    tok_h1 = [i.txt.lower() for i in nlpspa(h1)]
+    tok_h1 = [i.text.lower() for i in nlpspa(h1)]
     jur_h1 = [_filter_jur(i) for i in tok_h1]
     jur_h1_clean = [i for i in jur_h1]
 
     # juri h1
-    if len(jur_h1_clean) == 1 : 
+    if len(jur_h1_clean) == 1:
         return jur_h1_clean[0]
-    if  len(jur_h1_clean) > 1 : 
+    if len(jur_h1_clean) > 1:
         return str(-1)
-
 
     # token filter sub_article
     tok_sub_article = [i.txt.lower() for i in nlpspa(sub_article)]
@@ -53,76 +55,39 @@ def predict_juridiction(struct_doc: list, nlpipe=None, nlpspa=None):
     jur_sub_article_clean = [i for i in jur_sub_article]
 
     # juri sub_article
-    if len(jur_sub_article_clean) == 1 : 
+    if len(jur_sub_article_clean) == 1:
         return jur_sub_article_clean[0]
-    if  len(jur_sub_article_clean) > 1 : 
+    if len(jur_sub_article_clean) > 1:
         return str(-2)
 
     return str(-3)
 
 
-
-
-
-
-    # cost in h1
-    if len(money_h1_clean) == 1:
-        return str(money_h1_clean[0])
-
-    if len(money_h1_clean) > 1:
-        return str(-2)
-
-
-
-    if "CFTC" in h1 : 
-        return "CFTC"
-
-
-    if "SEC" in h1 : 
-        return "SEC"
-
-    if "DOJ" in h1 : 
-        return "DOJ"
-
-
-    # ask all and get all possible response
-    ans = _ask_all(txt, nlpipe)
-
-    # group by ans, make cumulative sum of accuracy for eash ans and filter best ones
-    ll = _clean_ans(ans)
-
-    # reponse
-    resp = ", ".join([i["answer"] for i in ll])
-
-    return resp
-
-
 if __name__ == "__main__":
 
-    # # import
-    # from legal_doc_processing.utils import *
-    # from legal_doc_processing.press_release.utils import *
-    # from legal_doc_processing.press_release.segmentation.structure import (
-    #     structure_press_release,
-    # )
+    # import
+    from legal_doc_processing.utils import get_pipeline, get_spacy, get_label_
+    from legal_doc_processing.press_release.utils import press_release_X_y
+    from legal_doc_processing.press_release.segmentation.structure import (
+        structure_press_release,
+    )
 
-    # # pipe
-    # nlpipe = get_pipeline()
+    # laod
+    nlpipe = get_pipeline()
+    nlpspa = get_spacy()
 
-    # # struct_doc_list
-    # press_txt_list = load_press_release_text_list()
-    # struct_doc_list = [structure_press_release(i) for i in press_txt_list]
+    # structured_press_release_r
+    df = press_release_X_y(features="cost")
+    df["structured_txt"] = [structure_press_release(i) for i in df.txt.values]
 
-    # # test one
-    # struct_doc = struct_doc_list[0]
-
-    # all_ans_h1 = _ask_all(struct_doc["h1"], nlpipe)
-    # all_ans_h2 = _ask_all(struct_doc["h2"], nlpipe)
-    # all_ans_article = _ask_all(struct_doc["article"], nlpipe)
-
-    # ans = predict_plaintiff(struct_doc, nlpipe)
-
-    # # test others
-    # ans_list = [predict_plaintiff(p, nlpipe) for p in struct_doc_list]
-    # clean_ans_list = [[d["answer"] for d in ll] for ll in ans_list]
-    # clean_ans_list = [", ".join(ll) for ll in clean_ans_list]
+    # one
+    one = df.iloc[0, :]
+    # one features
+    cost = one.cost
+    one_struct = struct_doc = one.structured_txt
+    one_h1 = one_struct["h1"]
+    one_article = one_struct["article"]
+    sub_one_article = "\n".join(one_article.split("\n")[:2])
+    # pred_h1  ⁼ predict_juridiction(one_h1)
+    # pred_sub_article  ⁼ predict_juridiction(one_h1)
+    pred = predict_juridiction(one_struct, nlpipe=nlpipe, nlpspa=nlpspa)

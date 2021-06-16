417c441ff7301fcdc00b19cc1c175b4ce684ec6f
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-06-16 16:06:39 +0200

    update

--- utils/boot.py
+++ utils/boot.py
@@ -1,11 +1,11 @@
-import legal_doc_processing
-
 from legal_doc_processing.pr import read_PressRelease as rr
 
 ob = rr("./data/boot/press_release.txt")
-ob.predict_all()
+pp = ob.predict_all()
+print(pp)
 
 from legal_doc_processing.ld import read_LegalDoc as rr
 
 ob = rr("./data/boot/order.txt")
-ob.predict_all()
+pp = ob.predict_all()
+print(pp)

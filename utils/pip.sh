d9ebed61dd567547fbcfa1e65b0c4ba67d8e268c
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-06-25 15:50:22 +0200

    update

--- utils/pip.sh
+++ utils/pip.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 docker build --no-cache -f utils/Dockerfile.pip -t python3-pip:latest .
-docker run -ti python3-pip:latest ipython
+docker run -ti python3-pip:latest /bin/bash ipython

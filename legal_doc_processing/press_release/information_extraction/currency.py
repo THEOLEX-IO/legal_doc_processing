72eb8f7a7aac84b9ed4db6565cac3da50d3839aa
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-07-02 22:01:23 +0200

    fix currency

--- legal_doc_processing/press_release/information_extraction/currency.py
+++ legal_doc_processing/press_release/information_extraction/currency.py
@@ -1,8 +1,10 @@
 def predict_currency(obj: dict) -> list:
     """ """
 
+    curr_dict = {"€": "EUR", "$": "USD", "£": "GBP"}
+
     for k in ["€", "$", "£"]:
         if (k in obj["h1"]) or (k in obj["abstract"]):
-            return [(k, 1)]
+            return [(curr_dict[k], 1)]
 
     return [(-1, -1)]

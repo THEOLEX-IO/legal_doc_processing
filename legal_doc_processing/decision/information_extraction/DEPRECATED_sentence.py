fb34d5dcc8eeb2af2816ca39c93dfa6b0dcae0d7
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-07-02 22:31:09 +0200

    del deprecated

--- legal_doc_processing/decision/information_extraction/DEPRECATED_sentence.py
+++
@@ -1,4 +0,0 @@
-def predict_sentence(press_obj: dict, legal_obj: dict) -> list:
-    """ """
-
-    return [(-1, -1)]
--- legal_doc_processing/legal_doc/information_extraction/DEPRECATED_juridiction.py
+++
@@ -1,4 +0,0 @@
-def predict_juridiction(obj: dict) -> list:
-    """ """
-
-    return [("--None--", -1)]

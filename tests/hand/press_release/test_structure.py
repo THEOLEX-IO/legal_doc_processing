c4c8c04efedecb10e8ecb9152cf25ad038005fcd
AlexandreGazagnes <a_syoez@yahoo.fr>
2021-07-08 12:59:07 +0200

    structure OK for 4 juridiction

--- tests/hand/press_release/test_structure.py
+++ tests/hand/press_release/test_structure.py
@@ -1,10 +1,25 @@
 from time import time
 from legal_doc_processing import logger
 
-from legal_doc_processing.utils import get_spacy, get_pipeline, get_label_
+from legal_doc_processing.utils import get_spacy
+
+from legal_doc_processing.press_release.structure.cftc import (
+    structure_press_release as structure_cftc,
+)
+from legal_doc_processing.press_release.structure.doj import (
+    structure_press_release as structure_doj,
+)
+
+from legal_doc_processing.press_release.structure.cfbp import (
+    structure_press_release as structure_cfbp,
+)
+
+from legal_doc_processing.press_release.structure.sec import (
+    structure_press_release as structure_sec,
+)
 
 
-def test_structure(juridiction, sample=0.99):
+def test_structure(juridiction, sample=0.99, nlspa=""):
     """ """
 
     assert juridiction in ["cftc", "cfbp", "doj", "sec"]
@@ -15,13 +30,18 @@ def test_structure(juridiction, sample=0.99):
     logger.info("called")
 
     # load
-    from legal_doc_processing.utils import get_pipeline, get_spacy
+    logger.info("load")
+    from legal_doc_processing.utils import get_spacy
 
-    nlpipe = get_pipeline()
-    nlspa = get_spacy()
-    nlspa.add_pipe("sentencizer")
+    if not nlspa:
+        nlspa = get_spacy()
+    try:
+        nlspa.add_pipe("sentencizer")
+    except Exception as e:
+        pass
 
     # dataframe
+    logger.info("dataframe")
     from legal_doc_processing.press_release.utils import press_release_X_y
 
     df = press_release_X_y(juridiction=juridiction, sample=sample)
@@ -29,21 +49,7 @@ def test_structure(juridiction, sample=0.99):
     df = df.loc[:, cols]
 
     # choose funct
-    from legal_doc_processing.press_release.structure.cftc import (
-        structure_press_release as structure_cftc,
-    )
-    from legal_doc_processing.press_release.structure.doj import (
-        structure_press_release as structure_doj,
-    )
-
-    from legal_doc_processing.press_release.structure.cfbp import (
-        structure_press_release as structure_cfbp,
-    )
-
-    from legal_doc_processing.press_release.structure.sec import (
-        structure_press_release as structure_sec,
-    )
-
+    logger.info("choose funct")
     if juridiction == "cftc":
         struct_funct = structure_cftc
     if juridiction == "doj":
@@ -54,15 +60,32 @@ def test_structure(juridiction, sample=0.99):
         struct_funct = structure_cfbp
 
     # structure
+    logger.info("structure")
     struct_ = lambda i: struct_funct(i, nlspa=nlspa)
     df["dd"] = df.press_release_text.apply(struct_)
 
     # extrcat cols
+    logger.info("extrcat cols")
     col_list = list(df.dd.iloc[0].keys())
     for col in col_list:
         df["dd_" + col] = df.dd.apply(lambda i: i.get(col, -42))
     df.drop("dd", inplace=True, axis=1)
 
     # save
+    logger.info("save")
     fn = f"./data/csv/structure_press_release_{juridiction}_{len(df)}_lines.csv"
-    df.to_csv("./data/csv/structure_press_release_cfbp.csv", index=False)
+    df.to_csv(fn, index=False)
+
+
+if __name__ == "__main__":
+
+    from time import time
+    from legal_doc_processing import logger
+
+    from legal_doc_processing.utils import get_spacy
+
+    nlspa = get_spacy()
+    nlspa.add_pipe("sentencizer")
+
+    auth_list = ["cftc", "cfbp", "doj", "sec"]
+    _ = [test_structure(i, sample=0.99, nlspa=nlspa) for i in auth_list]

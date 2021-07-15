32a126d2348652207d1eebe88b844e8d539b57ba
Sohar Samb <sokharsamb@hotmail.fr>
2021-07-15 13:32:06 +0000

    update

--- legal_doc_processing/press_release/country_of_violation/test.py
+++ legal_doc_processing/press_release/country_of_violation/test.py
@@ -3,12 +3,19 @@ from legal_doc_processing.press_release.country_of_violation.predict import pred
 
 
 
-
+from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer, AutoModelForQuestionAnswering
 
 from geopy.geocoders import Nominatim
 
- 
+#ask question about the residence of the defendant
+["where is the person who is charge from?",
+"Where does the person who is charge from",
+"Where is the country of violation?",
+]
+
+
+
 geolocator = Nominatim(user_agent = "geoapiExercises")
-location = geolocator.geocode("California")
+location = geolocator.geocode("Dakar")
 
 location._address

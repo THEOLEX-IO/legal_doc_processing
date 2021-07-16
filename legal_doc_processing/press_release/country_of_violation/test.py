17d9ab26ee507f84bd1b2f988916486124c76f7b
Sohar Samb <sokharsamb@hotmail.fr>
2021-07-16 00:49:18 +0000

    countries

--- legal_doc_processing/press_release/country_of_violation/test.py
+++ legal_doc_processing/press_release/country_of_violation/test.py
@@ -25,16 +25,44 @@
 
 # location._address
 
+import pdb
 
-if __name__ == "__main__":
+from legal_doc_processing import logger
 
-    # import
-    from legal_doc_processing.utils import get_label_, get_spa_pipe
-    from legal_doc_processing.utils import ask_all
+from legal_doc_processing.utils import uniquize as _u
+from legal_doc_processing.utils import get_label_
 
-    from legal_doc_processing.press_release.press_release import press_release_df
+from legal_doc_processing.press_release.country_of_violation.countries_list import (
+    countries_list,
+)
 
-    # spa and pipe
+
+def predict_country_of_violation(data: dict) -> list:
+    """ """
+
+    # authorities list
+    juridiction = data.juridiction
+    auth_list = data.feature_dict["extracted_authorities"].lower().split(";;")
+
+    # cfbp and cftc -> USA
+    for auth in ["cfbp", "cftc"]:
+        if juridiction in auth:
+            return [("United States", 1)]
+
+    # make sent list, and filter not cooperat in sent
+    sent_list = data.content_sents
+
+    # find the list of countries
+    countries_cands = list()
+    countries_list=[]
+    for sent in sent_list:
+        countries=ask_all(txt=sent, quest_pairs=[("What is the country of violation?", "country")], nlpipe=nlpipe)
+
+        countries_list.append(countries)
+
+        return countries_list
+
+   # spa and pipe
     nlpsa, nlpipe = get_spa_pipe()
 
     # make df
@@ -48,3 +76,76 @@ if __name__ == "__main__":
     pr = df.pr.iloc[0]
     pr.predict("extracted_authorities")
     data = pr.data
+
+countries=predict_country_of_violation(data)
+
+for cv in countries[0]:
+     if cv["score"]>0.7:
+         print(cv["answer"])
+
+
+
+
+
+if __name__ == "__main__":
+
+    # import
+    from legal_doc_processing.utils import get_label_, get_spa_pipe
+    from legal_doc_processing.utils import ask_all
+
+    from legal_doc_processing.press_release.press_release import press_release_df
+    from legal_doc_processing.press_release import country_of_violation
+
+
+
+
+    import pdb
+
+from legal_doc_processing import logger
+
+from legal_doc_processing.utils import uniquize as _u
+from legal_doc_processing.utils import get_label_
+
+from legal_doc_processing.press_release.country_of_violation.countries_list import (
+    countries_list,
+)
+
+
+def predict_country_of_violation(data: dict) -> list:
+    """ """
+
+    # authorities list
+    juridiction = data.juridiction
+    auth_list = data.feature_dict["extracted_authorities"].lower().split(";;")
+
+    # cfbp and cftc -> USA
+    for auth in ["cfbp", "cftc"]:
+        if juridiction in auth:
+            return [("United States", 1)]
+
+    # make sent list, and filter not cooperat in sent
+    sent_list = data.content_sents
+
+    # find the list of countries
+    countries_cands = list()
+    for sent in sent_list:
+        countries_cands.extend(get_label_(sent, "GPE", data.nlspa))
+    countries_lowered = _u([i.lower().strip() for i in countries_cands])
+
+    # filter
+    _countries_list = [i.lower().strip() for i in countries_list]
+    in_countries = lambda i: i.replace("the ", "").strip() in _countries_list
+    countries_filtered = [
+        i.replace("the", "").strip() for i in countries_lowered if in_countries(i)
+    ]
+
+    # if not countries_filtered:
+    # pdb.set_trace()
+
+    return [(i, 1) for i in countries_filtered]
+
+
+
+ 
+
+    ask_all(df.press_release_text, [("Where does the violation heppen?", "ok")], nlpipe=nlpipe)
